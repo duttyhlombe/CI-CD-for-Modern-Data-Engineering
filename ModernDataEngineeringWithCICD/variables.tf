08698dce54b34876a2da0b4ea7b57555c3a4d2de
Hlombe Malandela <hlombemclander@gmail.com>
2024-06-25 06:26:00 +0200

    Add files via upload

---
+++ ModernDataEngineeringWithCICD/variables.tf
@@ -0,0 +1,36 @@
+// variables.tf
+
+variable "resource_group_name" {
+  description = "The name of the resource group"
+  type        = string
+}
+
+variable "location" {
+  description = "The location/region of the resource"
+  type        = string
+}
+
+variable "tags" {
+  description = "Tags for the resources"
+  type        = map(string)
+}
+
+variable "storage_account_name" {
+  description = "The storage account name"
+  type        = string
+}
+
+variable "source_folder_name" {
+  description = "The source folder name"
+  type        = string
+}
+
+variable "destination_folder_name" {
+  description = "The destination folder name"
+  type        = string
+}
+
+variable "df_name" {
+  description = "The data factory name"
+  type        = string
+}
